d7adb2035a46ffe52cf73129da6dc5c0c2e30c41
Nyekefamo Bennett Benard <bennethade21@gmail.com>
2025-09-21 12:03:35 +0100

    welcome.vue updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,6 +19,8 @@
             })();
         </script>
 
+        <script src="https://js.puter.com/v2/" defer></script>
+
         {{-- Inline style to set the HTML background color based on our theme in app.css --}}
         <style>
             html {
